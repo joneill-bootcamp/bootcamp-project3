17c826a3cccb669ea7be6182da443d13b19398d3
John O'Neil <fluffywestie@tpg.com.au>
2020-07-20 00:21:23 +1000

    More Heroku Fixes...

--- server.js
+++ server.js
@@ -57,14 +57,25 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/"));
+// }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
